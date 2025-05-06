b1973de837edceaf122e6e5658ca4f0bd01a30c8
Guille <gaa1989@hotmail.com>
2025-05-06 13:50:47 -0500

    set promociones

--- client/src/components/cali_refugio_vf/pasta/lasagna.jsx
+++ client/src/components/cali_refugio_vf/pasta/lasagna.jsx
@@ -1,232 +1,336 @@
-import React from 'react';
-import { Component } from 'react';
-import '../../../App.css';
-import SaboresLasagna from './SaboresLasagna';
-import CombinadaIngredientes from './CombinadaIngredientes';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import { Component } from "react";
+import "../../../App.css";
+import SaboresLasagna from "./SaboresLasagna";
+import CombinadaIngredientes from "./CombinadaIngredientes";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Input,
+} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 class lasagna extends Component {
-    constructor(props){
-        super(props);    
-        this.state={
-            estado: 'inicio',
-            porcion: '',
-            saborpizza: '',
-            saborpizzaadicion: '',
-            modal: false,
-            indAdicional: '',
-            indicacionAdicional: '',
-            promo: false,
-            textoBoton: 'NAPOLITANA',
-            classSinConPromo: 'buttonPromocion',
-            costoLasagna: parseInt(process.env.REACT_APP_LASAGNA_COSTO),
-            classSalsa: 'buttonBolognesa'
-        }
-    }
-onClickBack = () => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      estado: "inicio",
+      porcion: "",
+      saborpizza: "",
+      saborpizzaadicion: "",
+      modal: false,
+      indAdicional: "",
+      indicacionAdicional: "",
+      promo: false,
+      textoBoton: "NAPOLITANA",
+      classSinConPromo: "buttonPromocion",
+      costoLasagna: parseInt(process.env.REACT_APP_LASAGNA_COSTO),
+      classSalsa: "buttonBolognesa",
+
+      promo2X1: false,
+      textoBotonPromo: "PROMO LSG 2X1",
+      classPromo2X1: "buttonSinPromo",
+      ajusteCostoPromo: 0,
+    };
+  }
+  onClickBack = () => {
     this.props.atrasMenuPersonal();
-}
+  };
 
-atrasPersonal = () => {
-    this.setState({estado: 'inicio'})
-}
+  atrasPersonal = () => {
+    this.setState({ estado: "inicio" });
+  };
 
-atrasPersonalSabor = (dato, porcion) => {
-    if(porcion === "saborpizza"){
-        this.setState({
-            saborpizza: dato,
-            estado: 'inicio',
-            saborpizzaadicion: ''
-        })
+  atrasPersonalSabor = (dato, porcion) => {
+    if (porcion === "saborpizza") {
+      this.setState({
+        saborpizza: dato,
+        estado: "inicio",
+        saborpizzaadicion: "",
+      });
     }
-    
-}
+  };
 
-atrasPersonalSaborAdicion = (dato, porcion) => {
-    if(porcion === "saborpizza"){
-        this.setState({saborpizzaadicion: dato + this.state.saborpizzaadicion,
-            estado: 'inicio'})
+  atrasPersonalSaborAdicion = (dato, porcion) => {
+    if (porcion === "saborpizza") {
+      this.setState({
+        saborpizzaadicion: dato + this.state.saborpizzaadicion,
+        estado: "inicio",
+      });
     }
-}
+  };
 
-sinIngredientesPizza = () => {
-    if(this.state.saborpizza === "") {
-        alert('No hay sabor')
-    }else{
-        this.setState({estado: "siningredientes"})
+  sinIngredientesPizza = () => {
+    if (this.state.saborpizza === "") {
+      alert("No hay sabor");
+    } else {
+      this.setState({ estado: "siningredientes" });
     }
-}
+  };
 
-adicionIngrediente = () => {
-    if(this.state.saborpizza === "") {
-        alert('No hay sabor')
-    }else{
-        this.setState({estado: "adicionentera"})
+  adicionIngrediente = () => {
+    if (this.state.saborpizza === "") {
+      alert("No hay sabor");
+    } else {
+      this.setState({ estado: "adicionentera" });
     }
-}
+  };
 
-agregaraCuenta = (dato, porcion) => {
-    //console.log(dato, porcion)     
-}
+  agregaraCuenta = (dato, porcion) => {
+    //console.log(dato, porcion)
+  };
 
-agregaraCuentaAdicionSinIng = (porcion, modificacionIng) => {
-    //console.log(porcion, this.state.mitaduno, modificacionIng)     
-}
+  agregaraCuentaAdicionSinIng = (porcion, modificacionIng) => {
+    //console.log(porcion, this.state.mitaduno, modificacionIng)
+  };
 
-agregarproducto = () => {
+  agregarproducto = () => {
     /*console.log("1/2: ", this.state.mitaduno)
     console.log("1/2: ", this.state.mitadunoadicion)
     console.log("2/2: ", this.state.mitaddos)
     console.log("2/2: ", this.state.mitaddosadicion)*/
-    //Abrimos modal    
-    if(this.state.saborpizza === ''){
-        alert('Completar sabores!!')
-    }else{
-        this.setState({
-            modal: !this.state.modal
-        })
-        //***//
-    }    
-}
+    //Abrimos modal
+    if (this.state.saborpizza === "") {
+      alert("Completar sabores!!");
+    } else {
+      this.setState({
+        modal: !this.state.modal,
+      });
+      //***//
+    }
+  };
 
-indicacionAdicional = (e) => {
+  indicacionAdicional = (e) => {
     this.setState({
-        indAdicional: e.target.value
-    })
-}
+      indAdicional: e.target.value,
+    });
+  };
 
-toggleModalCancelar = () => {
+  toggleModalCancelar = () => {
     //Toggle Modal
     this.setState({
-        modal: !this.state.modal
-    })
+      modal: !this.state.modal,
+    });
     //
-}
+  };
 
-toggleModalAceptar = () => {
+  toggleModalAceptar = () => {
     let pedidoPizza = [];
     let costoadicion = 0;
     let keyId = 0;
-    
+
     //Validamos costo ingredientes adicionales
-    if(this.state.saborpizzaadicion !== ''){
-        for (let i = 0; i < this.state.saborpizzaadicion.length; i++) {
-            if(this.state.saborpizzaadicion[i] === '+'){
-                costoadicion = costoadicion + 2000
-            }
+    if (this.state.saborpizzaadicion !== "") {
+      for (let i = 0; i < this.state.saborpizzaadicion.length; i++) {
+        if (this.state.saborpizzaadicion[i] === "+") {
+          costoadicion = costoadicion + 2000;
         }
+      }
     }
 
-    //Guardamos en local Storag 
-    let contPersonales = [JSON.parse(localStorage.getItem('Numero_Lasagnas'))]
+    //Guardamos en local Storag
+    let contPersonales = [JSON.parse(localStorage.getItem("Numero_Lasagnas"))];
 
-    if(contPersonales[0] === null){
-        //Guardamos en local Storage
-        pedidoPizza = { 'key_id' : 1,
-                    'tipo' : 'LASAGNA' + ' SALSA: ' + this.state.textoBoton,
-                    'sabor_lasagna' : this.state.saborpizza,
-                    'mod_sabor_lasagna' : this.state.saborpizzaadicion,
-                    'ind_lasagna_adicional': this.state.indAdicional,
-                    'costo_lasagna' : this.state.costoLasagna,
-                    'costo_adiciones_lasagna' : costoadicion,
-                    'id_pedido': 'Pedido_Lasagna_0'};
-        localStorage.setItem('Pedido_Lasagna_0', JSON.stringify(pedidoPizza))
-        localStorage.setItem('Numero_Lasagnas', JSON.stringify({'Numero': 1}))
-    }else{
-        pedidoPizza = { 'key_id' : contPersonales[0].Numero + 1,
-                    'tipo' : 'LASAGNA' + ' SALSA: ' + this.state.textoBoton,
-                    'sabor_lasagna' : this.state.saborpizza,
-                    'mod_sabor_lasagna' : this.state.saborpizzaadicion,
-                    'ind_lasagna_adicional': this.state.indAdicional,
-                    'costo_lasagna' : this.state.costoLasagna,
-                    'costo_adiciones_lasagna' : costoadicion,
-                    'id_pedido': `Pedido_Lasagna_${contPersonales[0].Numero}`};
-        localStorage.setItem(`Pedido_Lasagna_${contPersonales[0].Numero}`, JSON.stringify(pedidoPizza))
-        localStorage.setItem('Numero_Lasagnas', JSON.stringify({'Numero': contPersonales[0].Numero + 1}))
-    }  
+    if (contPersonales[0] === null) {
+      //Guardamos en local Storage
+      pedidoPizza = {
+        key_id: 1,
+        tipo: "LASAGNA" + " SALSA: " + this.state.textoBoton,
+        sabor_lasagna: this.state.saborpizza,
+        mod_sabor_lasagna: this.state.saborpizzaadicion,
+        ind_lasagna_adicional: this.state.indAdicional,
+        costo_lasagna: this.state.costoLasagna - this.state.ajusteCostoPromo,
+        costo_adiciones_lasagna: costoadicion,
+        id_pedido: "Pedido_Lasagna_0",
+      };
+      localStorage.setItem("Pedido_Lasagna_0", JSON.stringify(pedidoPizza));
+      localStorage.setItem("Numero_Lasagnas", JSON.stringify({ Numero: 1 }));
+    } else {
+      pedidoPizza = {
+        key_id: contPersonales[0].Numero + 1,
+        tipo: "LASAGNA" + " SALSA: " + this.state.textoBoton,
+        sabor_lasagna: this.state.saborpizza,
+        mod_sabor_lasagna: this.state.saborpizzaadicion,
+        ind_lasagna_adicional: this.state.indAdicional,
+        costo_lasagna: this.state.costoLasagna - this.state.ajusteCostoPromo,
+        costo_adiciones_lasagna: costoadicion,
+        id_pedido: `Pedido_Lasagna_${contPersonales[0].Numero}`,
+      };
+      localStorage.setItem(
+        `Pedido_Lasagna_${contPersonales[0].Numero}`,
+        JSON.stringify(pedidoPizza)
+      );
+      localStorage.setItem(
+        "Numero_Lasagnas",
+        JSON.stringify({ Numero: contPersonales[0].Numero + 1 })
+      );
+    }
 
     //Toggle Modal//
     this.setState({
-        modal: !this.state.modal
-    })
+      modal: !this.state.modal,
+    });
 
     //Atras
     this.props.atrasMenuPersonal();
-}
+  };
 
-esPromocion = () => {
-    if(this.state.promo === false){
-        this.setState({
-            promo: true,
-            textoBoton: 'QUESO',
-            classSalsa: 'buttonQueso',
-            costoLasagna: parseInt(process.env.REACT_APP_LASAGNA_CREMA)
-        })
-    }else(
-        this.setState({
-            promo: false,
-            textoBoton: 'NAPOLITANA',
-            classSalsa: 'buttonBolognesa',
-            costoLasagna: parseInt(process.env.REACT_APP_LASAGNA_COSTO)
-        })
-    ) 
-}
+  esPromocion = () => {
+    if (this.state.promo === false) {
+      this.setState({
+        promo: true,
+        textoBoton: "QUESO",
+        classSalsa: "buttonQueso",
+        ajusteCostoPromo: 0,
+        costoLasagna: parseInt(process.env.REACT_APP_LASAGNA_CREMA),
+      });
+    } else
+      this.setState({
+        promo: false,
+        textoBoton: "NAPOLITANA",
+        classSalsa: "buttonBolognesa",
+        costoLasagna: parseInt(process.env.REACT_APP_LASAGNA_COSTO),
+      });
+  };
 
-render(){
+  esPromocion2X1 = () => {
+    if (this.state.promo2X1 === false) {
+      this.setState({
+        promo2X1: true,
+        classPromo2X1: "buttonPromo",
+        ajusteCostoPromo: 5000,
+      });
+    } else
+      this.setState({
+        promo2X1: false,
+        classPromo2X1: "buttonSinPromo",
+        ajusteCostoPromo: 0,
+      });
+  };
+
+  render() {
     const opcionPizza = () => {
-        switch(this.state.estado) {
-            case "inicio": return(
+      switch (this.state.estado) {
+        case "inicio":
+          return (
             <>
-            <div>                
-                <div className='centrarButtonPromocionLasagna'>
-                    <p style={{textAlign: 'center'}}>LASAGNA - SALSA:</p>
-                    <p> </p>  
-                    <button className={this.state.classSalsa} onClick={this.esPromocion}>{this.state.textoBoton}</button>
-                </div>          
-                <div style={{display: 'flex', flexDirection: 'row'}}>
-                    <div className='adicioncompleta' onClick={this.adicionIngrediente}>                                
-                    </div>
-                    <div className='siningredientescompleta' onClick={this.sinIngredientesPizza}>                                
-                    </div>   
+              <div>
+                <div className="centrarButtonPromocionLasagna">
+                  <p style={{ textAlign: "center" }}>LASAGNA - SALSA:</p>
+                  <p> </p>
+                  <button
+                    className={this.state.classSalsa}
+                    onClick={this.esPromocion}
+                  >
+                    {this.state.textoBoton}
+                  </button>
+                  {this.state.textoBoton === "NAPOLITANA" && (
+                    <button
+                      className={this.state.classPromo2X1}
+                      onClick={this.esPromocion2X1}
+                    >
+                      {this.state.textoBotonPromo}
+                    </button>
+                  )}
                 </div>
-                <div className='pizzaforma'>                    
-                    <div>
-                        <div className='lasagna' onClick={() => (
-                        this.setState({estado: 'saborpizza'})
-                    )}>
-                        <p>{this.state.saborpizza} {this.state.saborpizzaadicion}</p>
-                        </div>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <div
+                    className="adicioncompleta"
+                    onClick={this.adicionIngrediente}
+                  ></div>
+                  <div
+                    className="siningredientescompleta"
+                    onClick={this.sinIngredientesPizza}
+                  ></div>
+                </div>
+                <div className="pizzaforma">
+                  <div>
+                    <div
+                      className="lasagna"
+                      onClick={() => this.setState({ estado: "saborpizza" })}
+                    >
+                      <p>
+                        {this.state.saborpizza} {this.state.saborpizzaadicion}
+                      </p>
                     </div>
-                    <button className='agregarapedido' onClick={this.agregarproducto}>AGREGAR</button>                       
-                </div>             
-                <p onClick={this.onClickBack} className='atras'>atras..</p>
-            </div>
+                  </div>
+                  <button
+                    className="agregarapedido"
+                    onClick={this.agregarproducto}
+                  >
+                    AGREGAR
+                  </button>
+                </div>
+                <p onClick={this.onClickBack} className="atras">
+                  atras..
+                </p>
+              </div>
 
-            <Modal isOpen={this.state.modal}>                        
-            <ModalHeader>Pedido LASAGNA</ModalHeader>
-            <ModalBody>
-                <p>Sabor:  <strong>{this.state.saborpizza}</strong></p>
-                <p>Adicion y/o Sin ingredientes: <strong>{this.state.saborpizzaadicion}</strong></p>
-                <Input value={this.state.indAdicional} onChange={this.indicacionAdicional} placeholder="Indicacion Adicional" /><br></br>
-            </ModalBody>
-            <ModalFooter>
-                <Button color="success" onClick={this.toggleModalAceptar}>Agregar Pedido</Button> {'  '}
-                <Button color="danger" onClick={this.toggleModalCancelar}>Cancelar</Button>
-            </ModalFooter>
-            </Modal>
+              <Modal isOpen={this.state.modal}>
+                <ModalHeader>Pedido LASAGNA</ModalHeader>
+                <ModalBody>
+                  <p>
+                    Sabor: <strong>{this.state.saborpizza}</strong>
+                  </p>
+                  <p>
+                    Adicion y/o Sin ingredientes:{" "}
+                    <strong>{this.state.saborpizzaadicion}</strong>
+                  </p>
+                  <Input
+                    value={this.state.indAdicional}
+                    onChange={this.indicacionAdicional}
+                    placeholder="Indicacion Adicional"
+                  />
+                  <br></br>
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="success" onClick={this.toggleModalAceptar}>
+                    Agregar Pedido
+                  </Button>{" "}
+                  {"  "}
+                  <Button color="danger" onClick={this.toggleModalCancelar}>
+                    Cancelar
+                  </Button>
+                </ModalFooter>
+              </Modal>
             </>
-            )
-            case "saborpizza": return (<SaboresLasagna porcion="saborpizza" atrasMenuPersonal={this.atrasPersonal} atrasMenuPersonalSabor={this.atrasPersonalSabor} cuentamitad={this.agregaraCuenta}/>)
-            case "adicionentera":return (<CombinadaIngredientes porcion="saborpizza" atrasMenuPersonal={this.atrasPersonal} atrasMenuPersonalSabor={this.atrasPersonalSaborAdicion} cuentamitadmodificacion={this.agregaraCuentaAdicionSinIng}/>)
-            case "siningredientes":return (<CombinadaIngredientes porcion="saborpizza" opcioncuarto='ingredientemenos' atrasMenuPersonal={this.atrasPersonal} atrasMenuPersonalSabor={this.atrasPersonalSaborAdicion} cuentamitadmodificacion={this.agregaraCuentaAdicionSinIng}/>)
-        }
-    }
-    return(
-        <div>{ opcionPizza()}</div>
-    )
-}
+          );
+        case "saborpizza":
+          return (
+            <SaboresLasagna
+              porcion="saborpizza"
+              atrasMenuPersonal={this.atrasPersonal}
+              atrasMenuPersonalSabor={this.atrasPersonalSabor}
+              cuentamitad={this.agregaraCuenta}
+            />
+          );
+        case "adicionentera":
+          return (
+            <CombinadaIngredientes
+              porcion="saborpizza"
+              atrasMenuPersonal={this.atrasPersonal}
+              atrasMenuPersonalSabor={this.atrasPersonalSaborAdicion}
+              cuentamitadmodificacion={this.agregaraCuentaAdicionSinIng}
+            />
+          );
+        case "siningredientes":
+          return (
+            <CombinadaIngredientes
+              porcion="saborpizza"
+              opcioncuarto="ingredientemenos"
+              atrasMenuPersonal={this.atrasPersonal}
+              atrasMenuPersonalSabor={this.atrasPersonalSaborAdicion}
+              cuentamitadmodificacion={this.agregaraCuentaAdicionSinIng}
+            />
+          );
+      }
+    };
+    return <div>{opcionPizza()}</div>;
+  }
 }
 
-export default lasagna
+export default lasagna;
